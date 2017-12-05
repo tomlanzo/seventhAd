4b3b5e48503761850a5f5336c4c7770c7127f05a
Antoine Tourasse <antoine.tourasse@gmail.com>
2017-12-05 12:39:09 +0100

    Added name to validation in player

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,5 @@ class Player < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :company
   validates :token, presence: true
-  validates :email, presence: true, if: :persisted?
+  validates :email, :name, presence: true, if: :persisted?
 end
