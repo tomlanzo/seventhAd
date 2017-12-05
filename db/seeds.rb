e03b803f3571d0132238e4b862827373f7221046
Elena Maroto <elena_maroto@hotmail.com>
2017-12-05 17:02:02 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cinemas = [
+   { name: 'Multiplexe Le Palace',
+     address: 'ZAC des Plaines de Figuerolles, 13500 Martigues'
+    },
+    { name: 'Le Renoir',
+     address: '24 Cours Mirabeu, 13100, Aix en Provence'
+    },
+    { name: 'Le Prado',
+     address: '36 Avenue du Prado, 13006 Marseille'
+    },
+  ]
+
+companies = [
+    { name: 'Haribo',
+      email: 'contact@haribo.com'
+    },
+    { name: 'Gran Frais',
+      email: 'contact@granfrais.com'
+    },
+    { name: 'Starbucks',
+      email: 'contact@starbucks.com'
+    },
+  ]
+
+games = [
+    { name: 'Le jeux des ours',
+      kind: 'Quizz'
+    },
+    { name: 'Cuisinon ensemble',
+      kind: 'Quizz'
+    },
+    { name: 'Le jeux des cafes',
+      kind: 'Quizz'
+    },
+  ]
+
+ questions = [
+    { position: '1',
+      title: '',
+      answer_1: '',
+      answer_2: '',
+      answer_3: '',
+      answer_4: '',
+      correct_answer: '',
+      duration: '',
+
+
+    },
+
+
+  ]
