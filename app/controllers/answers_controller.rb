c226bc2ae3df52ff811a9a83304b8d7e82305007
Elena Maroto <elena_maroto@hotmail.com>
2017-12-07 15:56:38 +0100

    we can answer questions!

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,9 +2,10 @@ class AnswersController < ApplicationController
 
   def create
     @answer = Answer.new(answer_params)
+    @answer.player = Player.find(params[:player_id])
+    @answer.question = Question.find(params[:question_id])
     @answer.game_session = @answer.player.game_session
-    @answer.correct = @answer.choice == @question.correct_answer
-
+    @answer.correct = @answer.choice == @answer.question.correct_answer
     if @answer.save
     #   redirect_to booking_path(@booking)
     else
@@ -16,6 +17,6 @@ class AnswersController < ApplicationController
   private
 
   def answer_params
-    params.require(:answer).permit(:choice, :player, :question)
+    params.require(:answer).permit(:choice)
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,8 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    @player = Player.find(params[:player_id])
+    @answer = Answer.new
   end
-
 
 end
