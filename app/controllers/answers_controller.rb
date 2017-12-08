c95a62702d453f1d5a8687cc1a0e11b2c10fa9b6
Elena Maroto <elena_maroto@hotmail.com>
2017-12-08 18:01:18 +0100

    wip

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,17 +6,12 @@ class AnswersController < ApplicationController
     @answer.question = Question.find(params[:question_id])
     @answer.game_session = @answer.player.game_session
     @answer.correct = @answer.choice == @answer.question.correct_answer
+    # raise
+      @next_question_start_at = params[:answer][:next_question_start_at]
+      @redirect_path = params[:answer][:redirect_path]
     if @answer.save
-      flash[:notice] = "Reponse #{@answer.choice} bien enregistrée"
-      # if @answer.question.position == 3
-      #  redirect_to edit_player_path(@answer.player)
-      # else
-      #   @next_question = Question.where(game: @answer.question.game,
-      #                    position: @answer.question.position += 1)
-      #   redirect_to player_question_path(@answer.player, @next_question.first.id)
-      # end
+      render 'questions/countdown'
     else
-      # render :new
       flash[:alert] ="La réponse n'a pas été enregistrée, reesayez, svp"
     end
 
