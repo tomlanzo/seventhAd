243115c5056bcefbf10d46c33059022125cabb21
Thomas Lanzoni <thomas.lanzoni@gmail.com>
2017-12-11 16:05:53 +0100

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "seventhadd.herokuapp.com"}
+  config.action_mailer.default_url_options = { host: "www.7thad.eu"}
 
 end
